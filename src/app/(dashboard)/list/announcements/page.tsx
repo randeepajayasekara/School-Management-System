4900a76f30a6e6bdd208851659de8b027edd8d2b
Randeepa Jayasekara <147414352+randeepajayasekara@users.noreply.github.com>
2025-01-21 21:42:11 +0530

    Dark Mode Color Change III

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -81,7 +81,7 @@ const AnnouncementListPage = () => {
   const renderRow = (item: Announcement) => (
     <tr
       key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-CustomPurpleLight"
+      className="border-b border-gray-200 dark:border-gray-800 even:bg-slate-50 dark:even:bg-slate-900 text-sm hover:bg-CustomPurpleLight dark:hover:bg-slate-800 duration-300"
     >
       <td className="flex items-center gap-4 p-4">{item.title}</td>
       <td>{item.class}</td>
@@ -100,7 +100,7 @@ const AnnouncementListPage = () => {
   );
 
   return (
-    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0 border border-gray-200">
+    <div className="bg-white dark:bg-slate-900 p-4 rounded-md flex-1 m-4 mt-0 border-2 border-gray-200 dark:border-gray-700">
       {/* TOP */}
       <div className="flex items-center justify-between">
         <h1 className="hidden md:block text-lg font-semibold">
@@ -111,7 +111,7 @@ const AnnouncementListPage = () => {
           <select
             value={selectedClass}
             onChange={handleClassFilterChange}
-            className="border rounded p-2 text-sm"
+            className="border rounded p-2 text-sm dark:bg-transparent dark:border-gray-700"
           >
             <option value="">All Classes</option>
             {[...new Set(announcementsData.map((announcement) => announcement.class))].map((className) => (
@@ -122,7 +122,7 @@ const AnnouncementListPage = () => {
           </select>
           <div className="flex items-center gap-4 self-end">
             <button
-              className="w-8 h-8 flex items-center justify-center rounded-full border"
+              className="w-8 h-8 flex items-center justify-center rounded-full border dark:border-gray-700"
               onClick={handleSort}
             >
               <ArrowsUpDownIcon className="w-5 h-5 text-gray-400" />
