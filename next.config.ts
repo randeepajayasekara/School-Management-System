1d1864f0421277b7525415bd88511f02865388a2
Randeepa Jayasekara <147414352+randeepajayasekara@users.noreply.github.com>
2025-01-21 21:38:07 +0530

    NavBar Dark Mode Improvements

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.pexels.com", "i.ibb.co", "firebasestorage.googleapis.com"],
+    domains: ["images.pexels.com", "i.ibb.co", "firebasestorage.googleapis.com", "i.postimg.cc"],
   },
 };
 
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,25 +12,25 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 dark:bg-[url('https://i.postimg.cc/K8kJychz/pexels-photo-6985132.jpg')] bg-no-repeat bg-cover bg-center backdrop-blur-md">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
         >
           <AcademicCapIcon className="h-5 w-5 animate-pulse" />
           <span className="hidden lg:block">
-            {(() => {
-              const hours = new Date().getHours();
-              if (hours < 12) return "Good Morning";
-              if (hours < 18) return "Good Afternoon";
-              return "Good Evening";
-            })()}
+        {(() => {
+          const hours = new Date().getHours();
+          if (hours < 12) return "Good Morning";
+          if (hours < 18) return "Good Afternoon";
+          return "Good Evening";
+        })()}
           </span>
         </Link>
         <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] dark:bg-black overflow-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
