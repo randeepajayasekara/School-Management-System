bff33998fc0c69a78c0389dcc5298609491c8a9c
Randeepa Jayasekara <147414352+randeepajayasekara@users.noreply.github.com>
2025-01-23 21:06:59 +0530

    Code Formatting II

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -7,10 +7,12 @@ const UserCard = ({ type }: { type: string }) => {
         <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">
           2024/25
         </span>
-        <EllipsisHorizontalIcon className="h-5 w-5"/>
+        <EllipsisHorizontalIcon className="h-5 w-5" />
       </div>
       <h1 className="text-2xl font-semibold my-4">1,234</h1>
-      <h2 className="capitalize text-sm font-medium text-gray-500 dark:text-gray-100">{type}s</h2>
+      <h2 className="capitalize text-sm font-medium text-gray-500 dark:text-gray-100">
+        {type}s
+      </h2>
     </div>
   );
 };
