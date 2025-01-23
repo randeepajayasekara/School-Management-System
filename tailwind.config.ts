d585026080fce9557eabf90f80f3833df47d9d61
Randeepa Jayasekara <147414352+randeepajayasekara@users.noreply.github.com>
2025-01-23 21:06:36 +0530

    Code Formatting I

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,33 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-  	extend: {
-  		backgroundImage: {
-  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-  		},
-  		colors: {
-  			CustomSky: '#C3EBFA',
-  			CustomSkyLight: '#EDF9FD',
-  			CustomPurple: '#CFCEFF',
-  			CustomPurpleLight: '#F1F0FF',
-  			CustomYellow: '#FAE27C',
-  			CustomYellowLight: '#FEFCE8'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+			},
+			colors: {
+				CustomSky: '#C3EBFA',
+				CustomSkyLight: '#EDF9FD',
+				CustomPurple: '#CFCEFF',
+				CustomPurpleLight: '#F1F0FF',
+				CustomYellow: '#FAE27C',
+				CustomYellowLight: '#FEFCE8'
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			}
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 };
 export default config;
